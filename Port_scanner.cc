71d267f72b8b9dd3b8cb3c1a3078c9a4c19b8dc7
MatthewDun <matthewwdunnavant@gmail.com>
2025-05-25 20:47:08 -0700

    Update Port_scanner.cc

--- Port_scanner.cc
+++ Port_scanner.cc
@@ -7,11 +7,22 @@ static bool port_is_open(const std::string& address, int port)
     return (sf::TcpSocket().connect(address, port) == sf::Socket::Done);
 }
 
+
 int main()
 {
-    std::cout << "Port 80 : ";
-    if (port_is_open("localhost", 80))
+    std::string address;
+    int port;
+    // Get the address.
+    std::cout << "Address: " << std::flush;
+    std::getline(std::cin, address);
+    // Get the port.
+    std::cout << "Port: " << std::flush;
+    std::cin >> port;
+    // Scan!
+    std::cout << "Scanning " << address << "...\n" << "Port " << port << " : ";
+    if (port_is_open(address, port))
         std::cout << "OPEN" << std::endl;
     else
         std::cout << "CLOSED" << std::endl;
     return 0;
+}
